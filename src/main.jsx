dca67b6a4d4cbc755cf874ba29f55e6b4ef56c28
maryadefisola-png <maryadefisola@gmail.com>
2026-08-19 11:00:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import TestSupabase from './TestSupabase.jsx'
+import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TestSupabase />
+    <App />
   </StrictMode>
 )
