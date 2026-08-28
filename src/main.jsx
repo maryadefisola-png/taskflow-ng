c2a294b947e113fdf483eaaf4f0b995505e523cc
maryadefisola-png <maryadefisola@gmail.com>
2026-08-28 23:14:26 +0100

    Restore admin route alongside user app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import GrowviaPolished from './GrowviaPolished.jsx'
+import Admin from './Admin.jsx'
 import './index.css'
+
+const RootApp = () => window.location.pathname.startsWith('/admin') ? <Admin /> : <GrowviaPolished />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GrowviaPolished />
+    <RootApp />
   </StrictMode>
 )
