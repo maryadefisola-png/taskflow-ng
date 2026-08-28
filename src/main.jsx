3bb6ff647f6ea336b545015c10bcbc83ad6434ac
maryadefisola-png <maryadefisola@gmail.com>
2026-08-28 07:34:14 +0100

    Include final Growvia UI polish with customer care

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import CustomerCare from './CustomerCare.jsx'
 import UserNavigation from './UserNavigation.jsx'
 import './index.css'
+import './GrowviaUI.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
