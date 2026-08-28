dd1ed09a491682f8c18fc309ed66211c69c23d10
maryadefisola-png <maryadefisola@gmail.com>
2026-08-28 23:06:41 +0100

    Use polished Growvia app as production entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import GrowviaComplete from './GrowviaComplete.jsx'
+import GrowviaPolished from './GrowviaPolished.jsx'
 import './index.css'
-import './GrowviaUI.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GrowviaComplete />
+    <GrowviaPolished />
   </StrictMode>
 )
