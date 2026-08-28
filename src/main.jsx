74955ccd658e9f2cd4cdd5b9dde9e3ab9bc36ac8
maryadefisola-png <maryadefisola@gmail.com>
2026-08-28 07:20:46 +0100

    Mount configurable customer care button

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import CustomerCare from './CustomerCare.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <CustomerCare />
   </StrictMode>
 )
