d8b7ca9449665e52af55e9e7e44fc43a69528867
maryadefisola-png <maryadefisola@gmail.com>
2026-08-28 07:33:58 +0100

    Mount Growvia user navigation with customer care

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CustomerCare from './CustomerCare.jsx'
+import UserNavigation from './UserNavigation.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <UserNavigation />
     <CustomerCare />
   </StrictMode>
 )
