183d90f8d744985b246e5d6ec6afe2e2f8cda2f7
maryadefisola-png <maryadefisola@gmail.com>
2026-08-29 01:07:08 +0100

    Refresh Growvia PWA cache for new icons

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "growvia-v1"
-const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg"]
+const CACHE = "growvia-v2"
+const APP_SHELL = ["/", "/manifest.webmanifest", "/icon.svg", "/icon-192.svg", "/icon-512.svg"]
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(() => self.skipWaiting())) })
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())) })
 self.addEventListener("fetch", event => {
